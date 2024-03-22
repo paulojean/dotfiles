49424afc9433003dfa219f498d7d7665e8ac214f
Paulo Sousa <pauloj10@gmail.com>
2024-03-22 09:25:56 +0100

    Use nvidia
    And do not hibernate on lid close.

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -65,6 +65,14 @@ in
   #   nvidiaBusId = "PCI:1:0:0";
   # };
 
+  # Enable OpenGL
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
@@ -145,7 +153,7 @@ in
     curl whois kitty
     harfbuzzFull
     # bash-completion
-    exa lsd bat ack silver-searcher fd gnugrep pstree bottom
+    eza lsd bat ack silver-searcher fd gnugrep pstree bottom
     openssl wireshark
     udiskie usbutils ipad_charge
     firefox chromium bitwarden vlc # tmux
@@ -220,17 +228,15 @@ in
     kind
     kubectl
   ];
-  # nixpkgs.config.permittedInsecurePackages = [
-  #   "p7zip-16.02"
-  #   "openssl-1.0.2u"
-  # ];
 
   programs = {
     light.enable = true;
     adb.enable = true;
+    steam.enable = true;
   };
+  programs.nm-applet.enable = true;
 
-  fonts.fonts = with pkgs; [
+  fonts.packages = with pkgs; [
     powerline-fonts
     font-awesome             # icons
     (nerdfonts.override {    # Nerdfont Icons override
@@ -279,13 +285,16 @@ in
      ];
     };
 
-    layout = "us";
-    xkbVariant = "intl";
-    xkbOptions = "eurosign:e, caps:ctrl_modifier";
+    xkb = {
+      layout = "us";
+      options = "eurosign:e, caps:ctrl_modifier";
+      variant = "intl";
+    };
 
     # "displaylink" to make nix recognize HDMI entry
     # videoDrivers = [ "displaylink" "modesetting" ];
-    videoDrivers = [ "modesetting" ];
+    # videoDrivers = [ "modesetting" ];
+    videoDrivers = ["nvidia"];
 
     # Enable touchpad support.
     libinput = {
@@ -300,6 +309,42 @@ in
     };
   };
 
+
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    powerManagement.enable = false;
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  hardware.nvidia.prime = {
+    sync.enable = true;
+    # Make sure to use the correct Bus ID values for your system!
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:1:0:0";
+  };
+
   # services.emacs.enable = true;
 
   # services.clipmenu.enable = true;
@@ -312,21 +357,22 @@ in
     timeout = 10;
   };
 
-  services.logind.extraConfig = ''
-    HandleSuspendKey=hibernate
-    HandleLidSwitch=hibernate
-  '';
+  services.logind.lidSwitch = "ignore";
+  # services.logind.extraConfig = ''
+  #   HandleSuspendKey=hibernate
+  #   HandleLidSwitch=hibernate
+  # '';
 
-  services.picom = {
-    enable = true;
-    inactiveOpacity = 0.9;
-    opacityRules = [
-      "100:class_g = 'Rofi'"
-      "100:class_g = 'Firefox'"
-      "100:class_g = 'i3lock'"
-      "100:class_g = 'i3lock-fancy'"
-    ];
-  };
+  # services.picom = {
+  #   enable = true;
+  #   inactiveOpacity = 0.9;
+  #   opacityRules = [
+  #     "100:class_g = 'Rofi'"
+  #     "100:class_g = 'Firefox'"
+  #     "100:class_g = 'i3lock'"
+  #     "100:class_g = 'i3lock-fancy'"
+  #   ];
+  # };
 
   environment.etc = {
     "modprobe.d/blacklist-nouveau.conf".text = ''
@@ -335,17 +381,17 @@ in
     '';
   };
 
-  services.udev.extraRules = ''
-    # https://wiki.archlinux.org/title/Hybrid_graphics#Using_udev_rules
-    # Remove NVIDIA USB xHCI Host Controller devices, if present
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
-    # Remove NVIDIA USB Type-C UCSI devices, if present
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
-    # Remove NVIDIA Audio devices, if present
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
-    # Remove NVIDIA VGA/3D controller devices
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
-  '';
+  # services.udev.extraRules = ''
+  #   # https://wiki.archlinux.org/title/Hybrid_graphics#Using_udev_rules
+  #   # Remove NVIDIA USB xHCI Host Controller devices, if present
+  #   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+  #   # Remove NVIDIA USB Type-C UCSI devices, if present
+  #   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+  #   # Remove NVIDIA Audio devices, if present
+  #   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+  #   # Remove NVIDIA VGA/3D controller devices
+  #   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+  # '';
 
   systemd.extraConfig = ''
     DefaultTimeoutStartSec=30s
